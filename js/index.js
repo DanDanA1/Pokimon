d6f6d14cdcd03d0e28a89e644d8cdeefe3b14d1a
alexandra <alexandramartingonzalez@gmail.com>
2025-03-25 15:29:22 -0500

    Segundo cargo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-h
+function Loguear() {
+    let username = document.getElementById('username').value
+    let password = document.getElementById('clave').value
+
+    if(username == 'Rodolfo' && password == '123456'){
+        alert('Usuario valido')
+    }else{
+        alert('usuario incorrecto')
+    }  
+
+}
